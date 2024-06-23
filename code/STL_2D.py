0caa1222dcbf65c4b6b4b23cdca185afeff31fe0
KET <keentong@foxmail.com>
2024-06-23 16:06:53 +0800

    Fix: updated STL_2D.py with correct logic

--- code/STL_2D.py
+++ code/STL_2D.py
@@ -1,89 +1,149 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 from statsmodels.tsa.seasonal import STL
 from tqdm import tqdm
+import multiprocessing as mp
 from osgeo import gdal
 import os
+import concurrent.futures
 
-# Processing time series images (TSI) into df
-def STL_2D(TSI: np.ndarray,**kwargs):
+def process_pixel(args):
+    timeseries, kwargs = args
+    STL_result = STL(timeseries, **kwargs).fit()
+    return STL_result.trend, STL_result.seasonal, STL_result.resid
+
+def STL_2D(TSI: np.ndarray, cpu_count=mp.cpu_count(), **kwargs):
     '''
     Input:
-    TSM: A 3D NumPy array where each dimension contains information for (timeslots,  columns,  rows), respectively.
+    TSI: A 3D NumPy array where each dimension contains information for (timeslots, columns, rows), respectively.
+    cpu_count: Number of CPU processors, default is to call all processors.
     See statsmodels.tsa.seasonal.STL for other parameters (https://www.statsmodels.org/devel/generated/statsmodels.tsa.seasonal.STL.html).
 
     Output:
     trend_df: DataFrame of trend, indexed by timeslots, each column representing a pixel.
     seasonal_df: DataFrame of season, indexed by timeslots, each column representing a pixel.
     resid_df: DataFrame of residual, indexed by timeslots, each column representing a pixel.
-
     '''
-    trend_list = []
-    seasonal_list = []
-    resid_list = []
+    timeslots, columns, rows = TSI.shape
 
-    timeslots, rows, columns = TSI.shape
-    for i in tqdm(range(columns)):
-        for j in range(rows):
-            STL_result = STL(TSI[:,i,j], **kwargs).fit()
-            
-            trend_list.append(STL_result.trend)
-            seasonal_list.append(STL_result.seasonal)
-            resid_list.append(STL_result.resid)
+    # Prepare arguments for parallel processing
+    args = [(TSI[:, i, j], kwargs) for i in range(columns) for j in range(rows)]
 
-    trend_df = pd.DataFrame(trend_list, columns=TSI[:,0,0])
-    seasonal_df = pd.DataFrame(seasonal_list, columns=TSI[:,0,0])
-    resid_df = pd.DataFrame(resid_list, columns=TSI[:,0,0])
+    # Use multiprocessing to parallelize the process
+    with mp.Pool(cpu_count) as pool:
+        results = list(tqdm(pool.imap(process_pixel, args), total=columns*rows))
 
-    trend_df = trend_df.reset_index(drop=True)
-    seasonal_df = seasonal_df.reset_index(drop=True)
-    resid_df = resid_df.reset_index(drop=True)
+    trend_list, seasonal_list, resid_list = zip(*results)
 
-    
+    trend_array = np.array(trend_list).reshape(columns, rows, timeslots).transpose(2, 0, 1)
+    seasonal_array = np.array(seasonal_list).reshape(columns, rows, timeslots).transpose(2, 0, 1)
+    resid_array = np.array(resid_list).reshape(columns, rows, timeslots).transpose(2, 0, 1)
+
+    trend_df = pd.DataFrame(trend_array.reshape(timeslots, -1))
+    seasonal_df = pd.DataFrame(seasonal_array.reshape(timeslots, -1))
+    resid_df = pd.DataFrame(resid_array.reshape(timeslots, -1))
+
     return trend_df, seasonal_df, resid_df
 
 
-def Result2Image(df: pd.DataFrame, rows, columns, output_folder):
+
+def save_image(time, array, rows, columns, output_folder):
+    file_name = str(time) + '.tif'
+    file_path = os.path.join(output_folder, file_name)
+    driver = gdal.GetDriverByName('GTiff')
+    dataset = driver.Create(file_path, columns, rows, 1, gdal.GDT_Float32)
+    image = dataset.GetRasterBand(1)
+    image.WriteArray(array)
+    image.FlushCache()
+    dataset = None
+
+def Result2Image(df: pd.DataFrame, rows, columns, output_folder, max_threads=None):
     '''
     df: DataFrame of trend/season/residual/anomaly, which is output by function STL_2D()
     rows, columns: The number of the output images' rows and columns
     output_folder: Folder for output
-
+    max_threads: Maximum number of threads to use (default: None, uses system default)
+    
     The names of output images are timeslots
     '''
-
+    
     timeslots = df.shape[1]
-    iteration = zip(list(range(timeslots)), df.columns.tolist())
-    for index, time in tqdm(iteration):
+    iteration = zip(list(range(timeslots)), df.index.tolist())
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+        futures = []
+        for index, time in tqdm(iteration):
+            array = df.iloc[index, :].values.reshape(rows, columns)
+            futures.append(executor.submit(save_image, time, array, rows, columns, output_folder))
         
-        array = df.iloc[:, index].values.reshape(rows, columns)
-
-
-        file_name = str(time) + '.tif'
-        file_path = os.path.join(output_folder, file_name)
-
-        driver = gdal.GetDriverByName('GTiff')
-        dataset = driver.Create(file_path, columns, rows, 1, gdal.GDT_Float32)
-        image = dataset.GetRasterBand(1)
-        image.WriteArray(array)
-        image.FlushCache()
-        dataset = None
+        # Ensure all threads complete
+        concurrent.futures.wait(futures)
 
     return
+
+
+def replace_image_values(time, array, template_image_path, output_folder):
+    file_name = str(time) + '.tif'
+    file_path = os.path.join(output_folder, file_name)
+    
+    template_dataset = gdal.Open(template_image_path, gdal.GA_ReadOnly)
+    driver = gdal.GetDriverByName('GTiff')
+    
+    dataset = driver.Create(file_path, template_dataset.RasterXSize, template_dataset.RasterYSize, 1, gdal.GDT_Float32)
+    dataset.SetGeoTransform(template_dataset.GetGeoTransform())
+    dataset.SetProjection(template_dataset.GetProjection())
+    
+    image = dataset.GetRasterBand(1)
+    image.WriteArray(array)
+    image.FlushCache()
+    dataset = None
+    template_dataset = None
+
+def Result2Image_withtemplate(df: pd.DataFrame, template_image_path, output_folder, max_threads=None):
+    '''
+    df: DataFrame of trend/season/residual/anomaly
+    template_image_path: Path to the template image which contains projection and geo-information
+    output_folder: Folder for output
+    max_threads: Maximum number of threads to use (default: None, uses system default)
+    
+    The names of output images are timeslots
+    '''
+    template_dataset = gdal.Open(template_image_path, gdal.GA_ReadOnly)
+    rows = template_dataset.RasterYSize
+    columns = template_dataset.RasterXSize
+    template_dataset = None
+
+
+    timeslots = df.shape[1]
+    iteration = zip(list(range(timeslots)), df.index.tolist())
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+        futures = []
+        for index, time in tqdm(iteration, total=df.shape[0]):
+            array = df.iloc[index, :].values.reshape(rows, columns)
+            futures.append(executor.submit(replace_image_values, time, array, template_image_path, output_folder))
+        
+        # Ensure all threads complete
+        concurrent.futures.wait(futures)
+
+    return
+
+
 
 def TukeyAnomalyDetection(df: pd.DataFrame):
     '''
     Detection of mutated residuals.
 
     input:
-    df: DataFrame of residual, which is output by function STL_2D()
+    df: DataFrame of residual/trend, which is output by function STL_2D()
 
     output:
     df_result: Anomaly detection results. -1 for abrupt decrease, 0 for stabilization, and 1 for abrupt increase.
     '''
-    df_result = df
-    for index, row in tqdm(df.iterrows()):
-        res_series = row.values
+    
+    df_result = df.copy()
+    for column_name, column_data in tqdm(df.items(), total=df.shape[1]):
+        res_series = column_data.values
 
         # find the thresholds
         Q1=np.percentile(res_series,25)
@@ -93,9 +153,9 @@ def TukeyAnomalyDetection(df: pd.DataFrame):
         lower=Q1-1.5*IQR
 
 
-        res_series[((res_series >= lower) & (res_series <= upper))] = 0
-        res_series[(res_series > upper)] = 1
-        res_series[(res_series < lower)] = -1
+        anomaly_flags = np.zeros_like(res_series)
+        anomaly_flags[res_series > upper] = 1
+        anomaly_flags[res_series < lower] = -1
 
-        df_result.loc[index] = res_series
+        df_result[column_name] = anomaly_flags
     return df_result
